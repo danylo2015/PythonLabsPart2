b209a3bd126410919634727d878c1790ceaad40a
Matsko Danylo <danylogit@gmail.com>
2024-03-12 08:46:57 +0200

    Added one more test case.

--- tests/test_max_diameter.py
+++ tests/test_max_diameter.py
@@ -16,5 +16,21 @@ class TestDiameter(unittest.TestCase):
         self.assertEqual(diameter(root), 6)
 
     def test_example_1(self):
-        root = BinaryTree(1)
-        self.assertEqual(diameter(root), 0)
+        root = BinaryTree(100)
+        root.left = BinaryTree(50)
+        root.right = BinaryTree(120)
+        root.left.left = BinaryTree(10)
+        root.right.right = BinaryTree(125)
+        root.right.left = BinaryTree(105)
+        root.left.right = BinaryTree(71)
+        root.left.left.left = BinaryTree(8)
+        root.left.left.right = BinaryTree(12)
+        root.left.left.left.left = BinaryTree(6)
+        root.left.left.left.left.left = BinaryTree(5)
+        root.left.left.right.right = BinaryTree(15)
+        root.left.left.right.right.right = BinaryTree(18)
+        root.left.left.right.right.right.left = BinaryTree(19)
+        root.left.left.right.right.right.left.left = BinaryTree(20)
+        self.assertEqual(diameter(root), 9)
+
+
